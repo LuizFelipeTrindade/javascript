419308202c6a26b7c196c5d1badd661889d0f290
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-02-07 19:33:10 -0300

    commit

---
+++ Modulo-C/Teste/script.js
@@ -0,0 +1,17 @@
+function carregar(){
+    var msg = document.getElementById('msg');
+    var img = document.getElementById ('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    msg.innerHTML = `Agora são ${hora} horas`;
+    if (hora >= 0 && hora < 12){
+        img.src = 'img1.png';
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'img2.png'
+        document.body.style.background = '#b9846f'
+    } else{
+        img.src = 'img3.png'
+        document.body.style.background = '#081F3A'
+    }
+} 
