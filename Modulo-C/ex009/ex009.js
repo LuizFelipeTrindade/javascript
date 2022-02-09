863146070028c1aef951fe84f7fa21364f8759da
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-02-09 17:56:35 -0300

    commit

---
+++ Modulo-C/ex009/ex009.js
@@ -0,0 +1,9 @@
+var media = 8;
+var nome = 'João';
+var turma = '1°A'
+if(media >= 6){
+    console.log(`O aluno ${nome} da turma ${turma} tirou ${media}, APROVADO`)
+} else {
+    console.log(`O aluno ${nome} da turma ${turma} tirou ${media}, REPROVADO`)
+}
+
---
+++ Modulo-C/ex010/ex010.js
@@ -0,0 +1,7 @@
+var velo = 57
+
+if(velo >= 60){
+    console.log(`Diriga com segurança, Você está a ${velo}km/h , você foi MULTADO(A)`)
+}else {
+    console.log('Diriga com segurança :)');
+}
