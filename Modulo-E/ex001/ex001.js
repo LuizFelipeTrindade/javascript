0d4099fed696aa0ce8a3363b02da9e7e7acedeaa
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-02-19 16:37:38 -0300

    commit

--- Modulo-E/ex001/ex001.js
+++ Modulo-E/ex001/ex001.js
@@ -1,8 +1,8 @@
 let num = [5, 8, 2];
 
-num.sort();
-
 num.push(12);
+
+num.sort();
 
 console.log(num);
 
@@ -10,8 +10,12 @@ console.log(`O vetor tem ${num.length} posições `);
 
 console.log(`O primeiro valor do vetor é ${num[0]}`);
 
-for(let pos=0; pos < num.length; pos++){
-    console.log(num[pos])
+let pos = num.indexOf(3) 
+
+if(pos == -1){
+    console.log('[ERRO]Valor não encontrado')
+}else {
+    console.log(`O valor está na posição ${pos}`)
 }
 
 /*console.log(num[2]);*/
---
+++ Modulo-E/ex003/ex003.js
@@ -0,0 +1,3 @@
+let pos = num.indexOf(8)
+
+console.log(`O valor 8 está na posicção `)
