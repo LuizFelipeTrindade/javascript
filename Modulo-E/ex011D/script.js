2caa5218cf060e3cd7d066532dea1131e530b047
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-02-28 16:11:15 -0300

    commit

--- Modulo-E/ex011D/script.js
+++ Modulo-E/ex011D/script.js
@@ -6,7 +6,7 @@ let res = document.querySelector('div#res')
 let valores = []
 
 function isNumero(n) {
-    if(Number(n) >= 1 && Number(n) >= 100){
+    if(Number(n) >= 1 && Number(n) <= 100){
         return true
     } else {
         return false
@@ -14,7 +14,7 @@ function isNumero(n) {
 }
 
 function inLista(n, l) {
-    if (l.indexOf(Number(n) != -1)) {
+    if (l.indexOf(Number(n)) != -1) {
        return true
     } else {
         return false
@@ -23,8 +23,40 @@ function inLista(n, l) {
 
 function adicionar() {
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        
+       valores.push(Number(num.value));
+       let item = document.createElement('option')
+       item.text = `Valor ${num.value} adicionado`
+       lista.appendChild(item)
+       res.innerHTML = ''
     } else {
         alert('Valor inválido ou já encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0) {
+        alert('Adicione valores')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot 
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${tot} números adicionados</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}. </p>`
+        res.innerHTML += `<p>A média dos vlaores digitados são ${media}</p>`
     }
 }
